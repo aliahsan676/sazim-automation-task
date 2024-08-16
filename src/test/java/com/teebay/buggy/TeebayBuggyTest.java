dcf285e3438d4f0e27d61d3d173dfe534112bc66
aliahsan676 <aliahsan676@gmail.com>
2024-08-17 03:07:34 +0600

    second commit

--- src/test/java/com/teebay/buggy/TeebayBuggyTest.java
+++ src/test/java/com/teebay/buggy/TeebayBuggyTest.java
@@ -1,55 +1,72 @@
 package com.teebay.buggy;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
 import java.util.Properties;
 
 public class TeebayBuggyTest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
+        // Load properties file from resources directory
         Properties props = new Properties();
-        FileInputStream file = new FileInputStream("config.properties");
-        props.load(file);
+        try (InputStream input = TeebayBuggyTest.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
 
+            // Load the properties from the file
+            props.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
+        // Retrieve the values
         String url = props.getProperty("WEBSITE_URL");
         String username = props.getProperty("USERNAME");
         String password = props.getProperty("PASSWORD");
 
-
+        // Set up the FirefoxDriver
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver = new FirefoxDriver();
 
-        try {
 
+        try {
+            // Navigate to the login page
             driver.get(url);
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
-
+            // Perform login
             WebElement webElement1 = driver.findElement(By.cssSelector("input[name='email']"));
             webElement1.clear();
             webElement1.sendKeys(username);
+
 
             WebElement webElement2 = driver.findElement(By.cssSelector("input[name='password']"));
             webElement2.clear();
             webElement2.sendKeys(password);
 
+
             driver.findElement(By.cssSelector("button[type='submit']")).click();
 
 
-        }finally {
+
+
+
+        } finally {
+
             driver.quit();
         }
-
     }
 
 }
